a195c5277031b588eeb17ca75f3f8cc466dbfa52
d2gex <daniel.garcia@d2garcia.com>
2024-06-25 18:15:27 +0200

    Document and apply lint to utpb_data_composition.R

---
+++ R/utpb_data_composition.R
@@ -0,0 +1,160 @@
+#' @title UtpbDataComposition  Class
+#'
+#' @description
+#' Custom class to build the catches and mean-weight matrcies for the UTPB database
+UtpbDataComposition <- R6::R6Class("UtpbDataComposition", public = list( # nolint
+  #' @field data a long dataframe with the length and weight data
+  data = NULL,
+  #' @field interval_col name of the column that will hold the size interval (as opposed to midpoints)
+  time_col = NULL,
+  #' @field size_col name of the column that holds lengths
+  size_col = NULL,
+  #' @field weight_col name of the column that holds weights
+  weight_col = NULL,
+  #' @field mean_weight_col name of the column that will hold the meanweights
+  mean_weight_col = NULL,
+  #' @field interval_col name of the column that will hold the size interval (as opposed to midpoints)
+  interval_col = NULL,
+  #' @field midpoint_col name of the column that will hold the length classes
+  midpoint_col = NULL,
+  #' @field freq_col name of the column that will hold the the length totals per class
+  freq_col = NULL,
+  #' @field composition_results name of the column that will hold the the length totals per class
+  composition_results = NULL,
+  linf = NULL,
+
+  # // @formatter:off
+  #' @description
+  #' Initialise the SpeciesDataComposition
+  #'
+  #' @param data a long dataframe with the length and weight data about a single species
+  #' @param time_col name of the column that holds dates
+  #' @param size_col name of the column that holds lengths
+  #' @param weight_col name of the column that holds weights
+  #' @param mean_weight_col name of the column that will hold mean weights
+  #' @param interval_col name of the column that will hold the size interval
+  #' (as opposed to midpoints)
+  #' @param midpoint_col name of the column that will hold the length classes
+  #' @param freq_col name of the column that will hold the the length totals per class
+  #' @param linf asymptotic length
+  #' @export
+  # // @formatter:on
+  initialize = function(data, sp_arte_context) {
+    self$data <- data
+    self$time_col <- sp_arte_context$time_col
+    self$size_col <- sp_arte_context$size_col
+    self$weight_col <- sp_arte_context$weight_col
+    self$mean_weight_col <- sp_arte_context$mean_weight_col
+    self$interval_col <- sp_arte_context$interval_col
+    self$midpoint_col <- sp_arte_context$midpoint_col
+    self$freq_col <- sp_arte_context$freq_col
+    self$linf <- sp_arte_context$linf
+    self$composition_results <- LCLWHolder$new()
+  },
+  # // @formatter:off
+  #' @description
+  #' Build a year-basis catch-at-length composition matrix when the weight column is
+  #' not debugged, therefore it could be NA
+  #'
+  #' @param bindwidth size of each length class
+  #' @param col_prefix prefix added to the each column representing a year
+  #' @param min_padding padding that is substracted to the minimum length class
+  #' @param up_to_linf boolean flag indicating whether the length classed should be capped
+  #' @export
+  # // @formatter:on
+  build_catch_at_length = function(bindwidth, col_prefix, min_padding, up_to_linf = TRUE) {
+    # Catch at length and mean weigh composition generator
+    composition <- CatchWeightComposition$new(
+      self$data,
+      self$size_col,
+      self$weight_col,
+      self$time_col,
+      self$interval_col,
+      self$midpoint_col,
+      self$freq_col,
+      self$linf
+    )
+    summary_catch <- composition$
+      generate_catch_at_length_composition(bindwidth, min_padding, up_to_linf)
+    summary_catch_long_wide <-
+      private$build_long_wide_variable_composition_matrix(
+        summary_catch,
+        col_prefix,
+        self$freq_col
+      )
+    self$composition_results$catch_long_t <- summary_catch_long_wide$long
+    self$composition_results$catch_wide_t <- summary_catch_long_wide$wide
+  },
+
+  # // @formatter:off
+  #' @description
+  #' Build a year-basis catch-at-length and mean-weight composition matrices both in long and wide format
+  #' (4 matrices, 2 for catch_at_length and 2 for mean-weight)
+  #'
+  #' @param bindwidth size of each length class
+  #' @param col_prefix prefix added to the each column representing a year
+  #' @param min_padding padding that is substracted to the minimum length class
+  #' @param up_to_linf boolean flag indicating whether the length classed should be capped
+  #' @export
+  # // @formatter:on
+  build_talla_and_weight_composition_matrices = function(bindwidth, col_prefix, min_padding, up_to_linf = TRUE) {
+    data <- self$data %>%
+      dplyr::filter_at(.vars = self$weight_col, not_na)
+    # Catch at length and mean weigh composition generator
+    composition <- CatchWeightComposition$new(
+      data,
+      self$size_col,
+      self$weight_col,
+      self$time_col,
+      self$interval_col,
+      self$midpoint_col,
+      self$freq_col,
+      self$linf
+    )
+    # (1) Generate catch and mean-weight
+    catch_mean_weight_at_length <-
+      composition$generate_catch_and_m.weight_at_length_composition(bindwidth, min_padding = 1, up_to_linf)
+
+    # (2) Build long and wide catch-at-length dataframe
+    catch_details <- catch_mean_weight_at_length %>% dplyr::select(-!!self$mean_weight_col)
+    summary_catch <- private$build_long_wide_variable_composition_matrix(catch_details, col_prefix, self$freq_col)
+    self$composition_results$catch_long_wt <- summary_catch$long
+    self$composition_results$catch_wide_wt <- summary_catch$wide
+
+    # (3) Build long and wide mean-weight-at-length dataframe
+    mean_weight_details <- catch_mean_weight_at_length %>% dplyr::select(-!!self$freq_col)
+    to_kg <- function(x) {
+      round(x / 100, 2)
+    }
+    summary_mean_weight <- private$build_long_wide_variable_composition_matrix(mean_weight_details,
+      col_prefix,
+      self$mean_weight_col,
+      converter_func = to_kg
+    )
+    self$composition_results$mwl <- summary_mean_weight$long
+    self$composition_results$mww <- summary_mean_weight$wide
+  }
+), private = list(
+  build_long_wide_variable_composition_matrix = function(data,
+                                                         col_prefix,
+                                                         variable,
+                                                         converter_func = NULL) {
+    # return long frame
+    summary_long <- data %>%
+      dplyr::arrange_at(.vars = c(self$time_col, self$interval_col))
+    if (!is.null(converter_func)) {
+      summary_long <- summary_long %>%
+        dplyr::mutate_at(.vars = variable, .funs = converter_func)
+    }
+
+    # return wide frame
+    summary_wide <- summary_long %>%
+      dplyr::pivot_wider(id_cols = -!!self$interval_col, names_from = !!self$time_col, values_from = !!variable)
+    summary_wide <- summary_wide %>%
+      dplyr::mutate_at(vars(-self$midpoint_col), replace_na, 0) %>%
+      dplyr::rename_with(~ stringr::str_c(col_prefix, .x), tidyr::matches("^\\d{4}$"))
+
+    summary_wide %>% assertr::assert(assertr::not_na, colnames(.), success_fun = NULL)
+    return(list(long = summary_long, wide = summary_wide))
+  }
+))
